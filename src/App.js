ba6b5c48c80f057eddfeebee39a19ee8b654c6ab
seriferst <seriferustemov@gmail.com>
2022-12-20 17:26:22 -0800

    react app for api data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { useState } from "react";
+import Axios from 'axios';
+
+const USER_URL = "https://jsonplaceholder.typicode.com/users";
 
 function App() {
+  const [names, setNames] = useState([]);
+  const [filteredItems, setFilteredItems] = useState([]);
+  const [searchItem, setSearchItem] = useState([]);
+
+  function showNames() {
+    Axios.get(USER_URL)
+        .then(response => {
+          let nameList = getNames(response);
+          setNames(nameList);
+        } )
+  }
+
+  function getNames(response) {
+    let nameList = [];
+    for(let i=0; i < response.data.length; i++){
+      nameList.push(response.data[i].name);
+    }
+    return nameList;
+  }
+
+  function searchName() {
+    let searchBox = document.getElementById('searchB');
+    searchBox.removeAttribute("hidden");
+  }
+
+  function findName(e) {
+    const searchValue = e.target.value; 
+    setSearchItem(searchValue);
+    let filteredList = names.filter(n =>  n?.toLowerCase().indexOf(searchValue.toLowerCase()) > -1);
+    filteredList = filteredList.map((item,key) => (<li key={key}>{item}</li>))
+    console.log(filteredList)
+    setFilteredItems(filteredList);
+  }
+
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={showNames}>Click This Button for User Names</button>
+      <button onClick={searchName}>Enable Filtering</button>
+      <div id="input">
+        <input onChange={findName} hidden type="text" id="searchB" placeholder="Enter a Name"></input>
+      </div>
+      <div>
+        <ul className='list'>
+          {
+            (searchItem.length > 0) ? filteredItems : names.map((item, key) => (<li key={key}>{item}</li>))
+          }
+        </ul>
+      </div>
+      
     </div>
   );
 }
